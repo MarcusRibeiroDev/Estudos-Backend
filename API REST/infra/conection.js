0fc063a916accb97a102fd58095de666aec7b883
MarcusRibeiroDev <marcus10dev@gmail.com>
2024-07-24 07:30:53 -0300

    Criando DB

---
+++ API REST/infra/conection.js
@@ -0,0 +1,5 @@
+import mysql from "mysql";
+
+const conection = mysql.createConnection({
+  host: "localhost",
+});
--- API REST/src/app.js
+++ API REST/src/app.js
@@ -5,10 +5,10 @@ app.use(express.json());
 //MOCK
 
 let teams = [
-  { id: 1, team: "Palmeiras" },
-  { id: 2, team: "Botafogo" },
-  { id: 3, team: "Flamengo" },
-  { id: 4, team: "Bahia" },
+  { id: 1, team: "Palmeiras", score: 80 },
+  { id: 2, team: "Botafogo", score: 65 },
+  { id: 3, team: "Flamengo", score: 76 },
+  { id: 4, team: "Bahia", score: 59 },
 ];
 
 // Functions
@@ -22,26 +22,39 @@ function searchIndexWithId(id) {
   return teamIndex ? teamIndex : "Não encontrado";
 }
 
+// Check da API
 app.get("/", (req, res) => {
-  res.send("Olá mundo!");
+  res.send("API de times do futebol Brasileiro!");
 });
 
+// Lista todos
 app.get("/times", (req, res) => {
-  res.status(200).send(teams);
+  res.json(teams);
 });
 
+// Lista individual
 app.get("/times/:id", (req, res) => {
   res.status(200).send(searchWithId(req.params.id));
 });
 
+// Cadastra/Posta individual
 app.post("/times", (req, res) => {
   teams.push(req.body);
-  res.status(201).send("Sucess");
+  res.json(teams);
 });
 
+// Deleta individual
 app.delete("/times/:id", (req, res) => {
   teams.splice(searchIndexWithId(req.params.id), 1);
-  res.status(202).send("Deleção feita com sucesso");
+  res.json(teams);
+});
+
+// Atualiza individual
+app.put("/times/:id", (req, res) => {
+  const index = searchIndexWithId(req.params.id);
+  teams[index].team = req.body.team;
+  teams[index].score = req.body.score;
+  res.json(teams);
 });
 
 export default app;
