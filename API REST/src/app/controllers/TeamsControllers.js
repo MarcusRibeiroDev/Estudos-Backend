d57dfc3880fd8043778e37e7c9b32e882ef4ccd4
MarcusRibeiroDev <marcus10dev@gmail.com>
2024-08-02 07:22:04 -0300

    Refatorando padrão MVC

--- API REST/src/app/controllers/TeamsControllers.js
+++ API REST/src/app/controllers/TeamsControllers.js
@@ -1,61 +1,29 @@
-import conection from "../database/conection.js";
 import TeamsRepositories from "../repositories/TeamsRepositories.js";
 
 class TeamsControllers {
   async index(req, res) {
-    const result = await TeamsRepositories.findAll();
+    let result = await TeamsRepositories.findAll();
     res.json(result);
   }
 
-  show(req, res) {
-    let id = req.params.id;
-    let sql = "SELECT * FROM db_team.teams WHERE id=?;";
-    conection.query(sql, id, (error, result) => {
-      let response = result[0];
-
-      if (error) {
-        res.status(404).json({ error: error });
-      } else {
-        res.status(200).json(response);
-      }
-    });
+  async show(req, res) {
+    let result = await TeamsRepositories.findById(req.params.id);
+    res.json(result);
   }
 
-  store(req, res) {
-    let newTeam = req.body;
-    let sql = "INSERT INTO db_team.teams SET ?;";
-    conection.query(sql, newTeam, (error, result) => {
-      if (error) {
-        res.status(404).json({ error: error });
-      } else {
-        res.status(201).json(result);
-      }
-    });
+  async store(req, res) {
+    let result = await TeamsRepositories.create(req.body);
+    res.json(result);
   }
 
-  delete(req, res) {
-    let id = req.params.id;
-    let sql = "DELETE FROM db_team.teams WHERE id=?;";
-    conection.query(sql, id, (error, result) => {
-      if (error) {
-        res.status(404).json({ error: error });
-      } else {
-        res.status(200).json(result);
-      }
-    });
+  async delete(req, res) {
+    let result = await TeamsRepositories.delete(req.params.id);
+    res.json(result);
   }
 
-  update(req, res) {
-    let id = req.params.id;
-    let newTeam = req.body;
-    let sql = "UPDATE db_team.teams SET ? WHERE id=?;";
-    conection.query(sql, [newTeam, id], (error, result) => {
-      if (error) {
-        res.status(404).json({ error: error });
-      } else {
-        res.status(200).json(result);
-      }
-    });
+  async update(req, res) {
+    let result = await TeamsRepositories.update(req.params.id, req.body);
+    res.json(result);
   }
 }
 
--- API REST/src/app/repositories/TeamsRepositories.js
+++ API REST/src/app/repositories/TeamsRepositories.js
@@ -1,24 +1,82 @@
+import { response } from "express";
 import conection from "../database/conection.js";
+
+function makeRequest(configReq) {
+  const { sql, message, id, newTeam } = configReq;
+  const promise = new Promise((resolve, reject) => {
+    conection.query(sql, (error, result) => {
+      if (error) return reject(message);
+      const res = JSON.parse(JSON.stringify(result));
+      return resolve(res);
+    });
+  });
+
+  return promise;
+}
 
 class TeamsRepositories {
   findAll() {
-    const sql = "SELECT * FROM db_team.teams;";
+    let sql = "SELECT * FROM db_team.teams;";
+    let message = "A busca de todas as equipes falhou";
+    return makeRequest({ sql, message });
+  }
+
+  findById(id) {
+    let sql = "SELECT * FROM db_team.teams WHERE id=?;";
     return new Promise((resolve, reject) => {
-      conection.query(sql, (error, result) => {
-        if (error) return reject("A busca de todos os times falhou");
-        const res = JSON.parse(JSON.stringify(result));
-        return resolve(res);
+      conection.query(sql, id, (error, result) => {
+        result = result[0];
+        if (error) {
+          return reject(`A busca individual do time com id ${id} falhou`);
+        } else {
+          const res = JSON.parse(JSON.stringify(result));
+          return resolve(res);
+        }
       });
     });
   }
 
-  findById() {}
+  create(newTeam) {
+    let sql = "INSERT INTO db_team.teams SET ?;";
+    return new Promise((resolve, reject) => {
+      conection.query(sql, newTeam, (error, result) => {
+        if (error) {
+          return reject(`A criação do time ${newTeam.team} falhou`);
+        } else {
+          const res = JSON.parse(JSON.stringify(result));
+          return resolve(res);
+        }
+      });
+    });
+  }
 
-  create() {}
+  delete(id) {
+    let sql = "DELETE FROM db_team.teams WHERE id=?;";
+    return new Promise((resolve, reject) => {
+      conection.query(sql, id, (error, result) => {
+        if (error) {
+          return reject(`A deleção do objeto com id ${id} falhou`);
+        } else {
+          const res = JSON.parse(JSON.stringify(result));
+          return resolve(res);
+        }
+      });
+    });
+  }
 
-  delete() {}
-
-  update() {}
+  update(id, newTeam) {
+    let sql = "UPDATE db_team.teams SET ? WHERE id=?;";
+    return new Promise((resolve, reject) => {
+      conection.query(sql, [newTeam, id], (error, result) => {
+        if (error) {
+          return reject(`A atualização do time com id ${id} falhou`);
+        } else {
+          const res = JSON.parse(JSON.stringify(result));
+          return resolve(res);
+        }
+      });
+    });
+  }
 }
 
 export default new TeamsRepositories();
