7056db6b855c0fb0cc18fb462e00ea1ab1d9da01
MarcusRibeiroDev <marcus10dev@gmail.com>
2024-08-07 07:22:42 -0300

    Refatorando o CRUD

--- API REST/src/app/database/conection.js
+++ API REST/src/app/database/conection.js
@@ -1,7 +1,7 @@
 import mysql from "mysql";
 
 const conection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   port: 3306,
   user: "root",
   password: "Marcus2020@",
@@ -10,6 +10,14 @@ const conection = mysql.createConnection({
 
 conection.connect();
 
+/**
+ * Executa uma instrução SQL e retorna uma Promise com o resultado.
+ *
+ * @param {string} sql - A instrução SQL a ser executada.
+ * @param {string} msg - Mensagem de feedback em caso de erro na execução da instrução SQL.
+ * @param {(string|Array<(string|Object)>)} [values=""] - Pode ser uma string representando um id ou um objeto com propriedades de um novo time. A segunda opção seria um array com os dois valores citados.
+ * @returns {Promise<Object>} - Uma Promise que resolve com o resultado da consulta SQL ou rejeita com a mensagem de erro especificada.
+ */
 export const makeRequest = (sql, msg, values = "") => {
   return new Promise((resolve, reject) => {
     conection.query(sql, values, (error, result) => {
--- API REST/src/app/repositories/TeamsRepositories.js
+++ API REST/src/app/repositories/TeamsRepositories.js
@@ -9,62 +9,38 @@ class TeamsRepositories {
     );
   }
 
-  // findById(id) {
-  //   let sql = "SELECT * FROM db_team.teams WHERE id=?;";
-  //   return new Promise((resolve, reject) => {
-  //     conection.query(sql, id, (error, result) => {
-  //       result = result[0];
-  //       if (error) {
-  //         return reject(`A busca individual do time com id ${id} falhou`);
-  //       } else {
-  //         const res = JSON.parse(JSON.stringify(result));
-  //         return resolve(res);
-  //       }
-  //     });
-  //   });
-  // }
+  findById(id) {
+    return makeRequest(
+      "SELECT * FROM db_team.teams WHERE id=?;",
+      "Não foi possível recuperar o time específico",
+      id
+    );
+  }
 
-  // create(newTeam) {
-  //   let sql = "INSERT INTO db_team.teams SET ?;";
-  //   return new Promise((resolve, reject) => {
-  //     conection.query(sql, newTeam, (error, result) => {
-  //       if (error) {
-  //         return reject(`A criação do time ${newTeam.team} falhou`);
-  //       } else {
-  //         const res = JSON.parse(JSON.stringify(result));
-  //         return resolve(res);
-  //       }
-  //     });
-  //   });
-  // }
+  create(newTeam) {
+    let sql = "INSERT INTO db_team.teams SET ?;";
+    return makeRequest(
+      "INSERT INTO db_team.teams SET ?;",
+      "Não foi possível cadastrar um novo time",
+      newTeam
+    );
+  }
 
-  // delete(id) {
-  //   let sql = "DELETE FROM db_team.teams WHERE id=?;";
-  //   return new Promise((resolve, reject) => {
-  //     conection.query(sql, id, (error, result) => {
-  //       if (error) {
-  //         return reject(`A deleção do objeto com id ${id} falhou`);
-  //       } else {
-  //         const res = JSON.parse(JSON.stringify(result));
-  //         return resolve(res);
-  //       }
-  //     });
-  //   });
-  // }
+  delete(id) {
+    return makeRequest(
+      "DELETE FROM db_team.teams WHERE id=?;",
+      "Não foi possível deletar o time",
+      id
+    );
+  }
 
-  // update(id, newTeam) {
-  //   let sql = "UPDATE db_team.teams SET ? WHERE id=?;";
-  //   return new Promise((resolve, reject) => {
-  //     conection.query(sql, [newTeam, id], (error, result) => {
-  //       if (error) {
-  //         return reject(`A atualização do time com id ${id} falhou`);
-  //       } else {
-  //         const res = JSON.parse(JSON.stringify(result));
-  //         return resolve(res);
-  //       }
-  //     });
-  //   });
-  // }
+  update(id, newTeam) {
+    return makeRequest(
+      "UPDATE db_team.teams SET ? WHERE id=?;",
+      "Não foi possível atualizar informações do time",
+      [newTeam, id]
+    );
+  }
 }
 
 export default new TeamsRepositories();
